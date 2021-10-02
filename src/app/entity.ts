2618510ffc1237264dc83985a827fb7cf9e1d7f3
Bhavik Kheni <kheni.bhavik@gmail.com>
2021-10-02 10:37:22 +0530

    create model

---
+++ src/app/entity.ts
@@ -0,0 +1,8 @@
+import { Product } from './model';
+
+export class Item {
+
+  product: Product;
+  quantity: number;
+
+}
---
+++ src/app/model.ts
@@ -0,0 +1,16 @@
+export class Product {
+  id: string;
+  name: string;
+  price: number;
+  photo: string;
+  availableProducts: number;
+  limitItem: number;
+  description: string;
+  charging: string;
+  date: string;
+  fingerprint: string;
+  sim: string;
+  network: string;
+
+
+}
